9e133dc074a94e1bd461f44590df065a9ac1cb45
Jose <ing.josetoro.2@gmail.com>
2024-01-21 09:56:46 -0500

    Delete unused assets and files

--- old-build/js/index.js
+++
@@ -1,6 +0,0 @@
-const navegationButton = document.querySelector('.header__navegation-button');
-const navegationMenu = document.querySelector('.header__navegation-menu');
-
-navegationButton.addEventListener('click', function () {
-	navegationMenu.classList.toggle('active');
-});
